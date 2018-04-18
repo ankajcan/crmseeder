8366471e2be1007cb75a0c9bd2f2a0e25a654939
alen <alen.ankajcan@gmail.com>
2018-04-18 11:04:27 +0200

    user account update

--- resources/views/account/avatar.blade.php
+++ resources/views/account/avatar.blade.php
@@ -1,5 +1,34 @@
 @if($avatar)
-    <img id="img-entity-avatar" src="{{ $avatar->url }}" class="img-lg"> <br> <br>
+    <div class="file-box avatar-container">
+        <div class="file">
+            <span class="corner"></span>
+            <div class="image" style="background-image:url('{{ $avatar->url }}')"></div>
+            <div class="file-name">
+                <div class="row">
+                    <div class="col-sm-10">
+                        asd
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @else
-    <img id="img-entity-avatar" src="http://via.placeholder.com/200x200?text=Avatar" class="img-lg"> <br> <br>
+    <div class="file-box avatar-container">
+        <div class="file">
+            <span class="corner"></span>
+            <div class="image" style="background-image:url('http://via.placeholder.com/200x200?text=Avatar')"></div>
+            <div class="file-name">
+                <div class="row">
+                    <div class="col-sm-10">
+                        asd
+                    </div>
+                    <div class="col-sm-2">
+                        asd
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endif
+
+
